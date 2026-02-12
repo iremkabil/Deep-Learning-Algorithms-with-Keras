2e84862327ac7e15a4f3d3b300b8563cf9d250ae
iremkabil <iremkabil54@gmail.com>
2026-02-12 15:00:43 +0300

    Update gan.py

--- gan.py
+++ gan.py
@@ -20,8 +20,26 @@ x_train = x_train / 255.0
 x_train = np.expand_dims(x_train, axis=-1)
 # %% Create Discriminator and Generator
 
-# discriminator modelini tanimla
+# gan parametreli
+z_dim = 100 # gurultu vektorunun bouyu
 
+# discriminator modelini tanimla
+def build_discriminator():
+    model = Sequential()
+    
+    # Conv2D: 64 filtre, 3x3 cekirdek (kernel), stride = 2, padding=same, activation =LeakyReLU
+    model.add(Conv2D(64, kernel_size = 3, strides = 2,padding ="same"), input_shape = (28,28,1))
+    model.add(LeakyReLU(alpha = 0.2))
+    # Conv2D: 128 filtre, 3x3 cekirdek (kernel), stride = 2, padding=same, activation =LeakyReLU
+    model.add(Conv2D(128, kernel_size = 3, strides=2, padding="same"))
+    model.add(LeakyReLU(alpha=0.2))
+    # flatten -> output layer
+    model.add(Flatten())
+    model.add(Dense(1, activation ="sigmoid")) # output layer
+    
+    #compile
+    model.compile(loss = "binary_crossentropy", optimizer=Adam(0.0002, 0.5),metrics = ["accuracy"])
+    
 # generator modelini tanimla
 
 # %% Create GAN model
