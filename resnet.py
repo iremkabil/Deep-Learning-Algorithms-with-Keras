d2850bf62a9e708de7c043a9c6b467efc42718cc
iremkabil <iremkabil54@gmail.com>
2026-02-18 16:43:29 +0300

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -39,10 +39,52 @@ def residual_block(x, filters, kernel_size = 3, stride = 1):
         shortcut = BatchNormalization()(shortcut)
     
     # residual baglanti: giris verisi ile cikis verisini toplayalim
+    x = Add()([x, shortcut])
+    x = Activation("relu")(x)
 
-
-
+    return x
 # %% create and compile resnet
+
+class ResNetModel(HyperModel):
+    def build(self, hp): # hp = hyperparameter tuning icin kullanilacak parametre
+    
+        inputs = Input(shape = (28,28,1))
+    
+        # 1. conv layer
+        x = Conv2D(filters = hp.Int("initial_filters", min_value = 32,max_value = 128,step =32),
+                  kernel_size=3, padding="same" ,activation="relu")(inputs)
+        x = BatchNormalization()(x)
+        
+        # residual blok ekleyelim
+        for i in range(hp.Int("num_blocks", min_value = 1, max_value = 3, step=1)):
+            x = residual_block(x, hp.Int("res_filters_" + str(i), min_value=32,max_value=128,step=32))
+        
+        # siniflandirma katmani
+        x = Flatten()(x)
+        x = Dense(128, activation = "relu")(x)
+        outputs = Dense(10, actvation = "softmax")(x)
+        
+        model = Model(inputs, outputs)
+        
+        # compile model
+        model.compile(optimizer =Adam(hp.Float("Learing_rate", min_value= 1e-4,max_value=1e-2,samping = "LOG")),
+                      loss = "categorical_crossentropy",
+                      metrics = ["accuracy"])
+        return model
+        
+
+
 
 
 # %% training, hyperparameter tuning and model evaluation
+
+
+
+
+
+
+
+
+
+
+
